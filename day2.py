e71086b97c68167a916bf4868f1ccfce6a0bfdc0
sahilkhandelwal954 <sahilkhandelwal954@gmail.com>
2024-05-14 14:48:39 +0530

    new file is uploaded

---
+++ day2.py
@@ -0,0 +1,69 @@
+# var1=10
+# print(var1)
+# var2=25.21
+# var3=True
+# var4="upflair"
+# var5='jecrc'
+# var6="""jecrc fondation"""
+
+# print(type(var1))
+# print(type(var2))
+# print(type(var3))
+# print(type(var4))
+# print(type(var5))
+# print(type(var6))
+# sahil=1
+# print(sahil)
+# rupees=25000000000000
+# print("i earn daily waise",rupees,"rupees")
+# paragraph="""hello we are from jecrc
+#                i am a student"""
+# print(paragraph)
+# college="jecrc foundation"
+# print(college.upper())
+# print(len(college))
+# print(college.count('c'))
+
+# print(college.endswith('n'))
+# print(college.endswith('s'))
+# print(college.startswith('j'))
+# print(college.endswith('foundation'))
+# college="jecrc foundation "
+
+# print(college)
+# # print(college.strip())
+# # print(college.lstrip())
+# # print(college.rstrip())
+# print(college[2])
+# print(college[6])
+# print(college[0:5])
+# print(college[-5:-1])
+# print(college[3:5])
+# print(college[-2:0])
+# print(college[0:5:1])
+college="JECRC"
+# print(college[-2:])
+# print(college[::-1])
+# print(college[4::-1])
+college='jecrc jaipur rajasthan'
+print(college[17:])
+print(college[-1:])
+print(college[::-1])
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
